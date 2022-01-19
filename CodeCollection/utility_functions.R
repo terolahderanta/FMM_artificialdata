ae61a79797a1d1885f2675e8a0057e8418341c2b
terolahderanta <tero.lahderanta@oulu.fi>
2022-01-19 10:24:58 +0200

    Create new project.

---
+++ CodeCollection/utility_functions.R
@@ -0,0 +1,7 @@
+get_cols <- function(){
+  return(c("blue","red","green","orange","hotpink","cyan","yellowgreen","purple",
+           "chocolate","darkred","yellow3","darkgreen","bisque4","magenta",
+           "royalblue","tomato4","steelblue1",
+           "seagreen4","orangered","darkblue","khaki3","lavender","deeppink2",
+           "coral3","beige","brown4","indianred1","lightgreen","orchid"))
+}
